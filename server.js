f56ea5aa070986cf4245f4365ce121a2d86c787b
Winston Malcolm <winstionmalcolm77@gmail.com>
2024-10-03 11:13:26 -0500

    create structure

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 const express = require("express");
+
+const messageRoutes = require("./routes/messageRoutes");
+const roomRoutes = require("./routes/roomRoutes");
+const userRoutes = require("./routes/userRoutes");
+
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.use("/message", messageRoutes);
+app.use("/room",roomRoutes);
+app.use("/user", userRoutes);
 
 app.listen(port,()=>console.log("listening on port ",port));
