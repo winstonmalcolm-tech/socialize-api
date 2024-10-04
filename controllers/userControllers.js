2497eda2bd327a661ae7c1987e91b2d9bd55c4e4
Winston Malcolm <winstionmalcolm77@gmail.com>
2024-10-04 18:40:36 -0500

    implement register feature

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,14 +1,46 @@
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const crypto = require("crypto");
+const bcrypt = require("bcryptjs");
+const mongoose = require("mongoose");
+const User = require("../models/userModel");
 
-
-
-const register = (req,res) => {
+const register = async (req,res, next) => {
     const { username, password } = req.body;
 
+    if (!username || !password) {
+        res.status(400);
+        next("Please fill all fields");
+        return;
+    }
+
     
+    const isUsernameAvailable = await User.findOne({username: username});
+
+    if (isUsernameAvailable) {
+        res.status(400);
+        next("Username already in use");
+        return;
+    }
+
+    if (password.length < 6) {
+        res.status(400);
+        next("Password length should be 6 and greater");
+        return;
+    }
+
+    const hashedPassword = await bcrypt.hash(password,10);
     
+    const user = new User({username: username, password: hashedPassword});
+    const result = await user.save();
+
+    if (result.errors) {
+        next("Error creating");
+        return;
+    }
+
+    res.status(201).json({msg: "Congratulation, Welcome"});
+
 }
 
 const login = (req,res) => {
